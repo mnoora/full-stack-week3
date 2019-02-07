f45891147f3b3485a83c486063e833cb831e5342
Noora Mäkelä <noora.m.makela@helsinki.fi>
2019-02-07 14:33:45 +0200

    heroku

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(bodyParser.json())
 
@@ -88,7 +91,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
     })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
