c2c11313dac7910892828fa91b270524e7ce4e69
Noora Mäkelä <noora.m.makela@helsinki.fi>
2019-02-07 14:21:41 +0200

    3.7 ja 3.8 valmis

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
+
+morgan.token('body', (req) => { return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
       {
